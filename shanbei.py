a17b4dd6d41cba337bfc0dc0005ddf3ffbabe0f4
winson <winsonyin123@gmail.com>
2016-06-04 19:20:38 +0800

    solve some error

--- shanbei.py
+++ shanbei.py
@@ -28,46 +28,46 @@ def compareTime(user, daytime):
 
 class RegisterHandler(BaseHandler):
     def get(self):
-        self.render("register.html")
+        return self.render("register.html")
 
     def post(self):
-        self.account = self.get_argument("account", "")
-        self.password = self.get_argument("password", "")
-        if self.account == "" or self.password == "":
+        account = self.get_argument("account", "")
+        password = self.get_argument("password", "")
+        if account == "" or password == "":
             return self.write("账号或密码为空")
 
-        if db.user.find({"account": self.account}).count() > 0:
+        if db.user.find({"account": account}).count() > 0:
             return self.write("用户名已存在")
 
-        db.user.insert({"account": self.account, "password": self.password, "user_status": None, "count": None, "read_status": "0", "today_time": None})
-        self.set_secure_cookie("user", self.account)
-        self.redirect("/")
+        db.user.insert({"account": account, "password": password, "user_status": None, "count": None, "read_status": "0", "today_time": None})
+        self.set_secure_cookie("user", account, expires_days=None)
+        return self.redirect("/")
 
 class LoginHandler(BaseHandler):
     def get(self):
         self.render("login.html")
 
     def post(self):
-        self.account = self.get_argument("account", "")
-        self.password = self.get_argument("password", "")
+        account = self.get_argument("account", "")
+        password = self.get_argument("password", "")
 
-        if self.account == "" or self.password == "":
+        if account == "" or password == "":
             return self.write("账号或密码为空")
 
-        self.user = db.user.find_one({"account": self.account}, {"account":1, "password":1})
+        user = db.user.find_one({"account": account}, {"account":1, "password":1})
 
-        if not self.user:
+        if not user:
             return self.write("账号不存在")
 
-        if self.user['password'] != self.password:
+        if user['password'] != password:
             return self.write("密码错误")
 
-        self.set_secure_cookie("user", self.user['account'], expires_days=None)
-        self.daytime = db.user.find_one({"account": self.account})
-        self.today = datetime.datetime.today()
-        self.date_time = datetime.datetime.strptime(self.daytime["today_time"], '%Y-%m-%d')
-        self.result = self.today - self.date_time
-        if int(self.result.days) != 0:
+        self.set_secure_cookie("user", user['account'], expires_days=None)
+        daytime = db.user.find_one({"account": account})
+        today = datetime.datetime.today()
+        date_time = datetime.datetime.strptime(daytime["today_time"], '%Y-%m-%d')
+        result = today - date_time
+        if int(result.days) != 0:
             return self.render("count.html")
         return self.redirect("/")
 
@@ -77,99 +77,97 @@ class IndexHandler(BaseHandler):
 
     @tornado.web.authenticated
     def get(self, *args, **kwargs):
-        self.user = self.get_current_user()
-        self.content = db.user.find_one({"account": self.user})
-        self.index = self.get_argument("index", "0")
+        user = self.get_current_user()
+        content = db.user.find_one({"account": user})
+        index = self.get_argument("index", "0")
 
-        if not self.content['user_status']:
+        if not content['user_status']:
             return self.render("select.html")
-        if not self.content['count']:
+        if not content['count']:
             return self.render("count.html")
 
-        if self.content['today_time'] == None:
-            db.user.update({"account": self.user}, {"$set": {"today_time": str(datetime.date.today())}})
+        if content['today_time'] == None:
+            db.user.update({"account": user}, {"$set": {"today_time": str(datetime.date.today())}})
             
-            if self.content['user_status'] == "4":
-                self.words = db.cet4.find().limit(int(self.content['count'])).skip(int(self.content['read_status']))
-                for self.word in self.words:
-                    self.daywords.append(self.word)
+            if content['user_status'] == "4":
+                words = db.cet4.find().limit(int(content['count'])).skip(int(content['read_status']))
+                for word in words:
+                    self.daywords.append(word)
 
-            if self.content['user_status'] == "6":
-                self.words = db.cet6.find().limit(int(self.content['count'])).skip(int(self.content['read_status']))
-                for self.word in self.words:
-                    self.daywords.append(self.word)
+            if content['user_status'] == "6":
+                words = db.cet6.find().limit(int(content['count'])).skip(int(content['read_status']))
+                for word in words:
+                    self.daywords.append(word)
         else:
-            self.content = db.user.find_one({"account": self.user})
-            self.result = compareTime(self.user, self.content["today_time"])
-            if self.result == 0:
+            content = db.user.find_one({"account": user})
+            result = compareTime(user, content["today_time"])
+            if result == 0:
 
                 del self.temp[:]
 
-                print self.daywords
+                if len(self.daywords) < int(content['count']):
+                    answer = int(content['count']) - len(self.daywords)
+                    if content['user_status'] == "4":
+                        words = db.cet4.find().limit(answer).skip(int(content['read_status']))
+                        for word in words:
+                            self.daywords.append(word)
 
-                if len(self.daywords) < int(self.content['count']):
-                    self.answer = int(self.content['count']) - len(self.daywords)
-                    if self.content['user_status'] == "4":
-                        self.words = db.cet4.find().limit(self.answer).skip(int(self.content['read_status']))
-                        for self.word in self.words:
-                            self.daywords.append(self.word)
+                    if content['user_status'] == "6":
+                        words = db.cet6.find().limit(answer).skip(int(content['read_status']))
+                        for word in words:
+                            self.daywords.append(word)
 
-                    if self.content['user_status'] == "6":
-                        self.words = db.cet6.find().limit(self.answer).skip(int(self.content['read_status']))
-                        for self.word in self.words:
-                            self.daywords.append(self.word)
-
-                for i in range(0, int(self.content['count'])):
+                for i in range(0, int(content['count'])):
                     self.temp.append(self.daywords[i])
 
                 del self.daywords[:]
 
-                for i in range(0, int(self.content['count'])):
+                for i in range(0, int(content['count'])):
                     self.daywords.append(self.temp[i])
             else:
                 del self.daywords[:]
-                if self.content['user_status'] == "4":
-                    self.words = db.cet4.find().limit(int(self.content['count'])).skip(int(self.content['read_status']))
-                    for self.word in self.words:
-                        self.daywords.append(self.word)
+                if content['user_status'] == "4":
+                    words = db.cet4.find().limit(int(content['count'])).skip(int(content['read_status']))
+                    for word in words:
+                        self.daywords.append(word)
 
-                if self.content['user_status'] == "6":
-                    self.words = db.cet6.find().limit(int(self.content['count'])).skip(int(self.content['read_status']))
-                    for self.word in self.words:
-                        self.daywords.append(self.word)
+                if content['user_status'] == "6":
+                    words = db.cet6.find().limit(int(content['count'])).skip(int(content['read_status']))
+                    for word in words:
+                        self.daywords.append(word)
 
-        self.shares = db.note.find({"$or": [{"share_status": "2"}, {"account": self.user}]})
+        shares = db.note.find({"$or": [{"share_status": "2"}, {"account": user}]})
 
-        if not self.shares:
-            self.exist = 0
+        if not shares:
+            exist = 0
         else:
-            self.exist = 1
+            exist = 1
 
-        if int(self.index) < len(self.daywords): 
-            self.num_index = int(self.index) 
-            print self.num_index
-            db.user.update({"account": self.user}, {"$set": {"read_status": self.index}})  
+        if int(index) < len(self.daywords): 
+            num_index = int(index) 
+            print num_index
+            db.user.update({"account": user}, {"$set": {"read_status": index}})  
         else:
-            self.num_index = len(self.daywords)-1
+            num_index = len(self.daywords)-1
 
 
-        if int(self.content['read_status']) != 0:
-            return self.render("index.html", words=self.daywords[self.num_index], content=int(self.content['read_status'])-1, exist=self.exist, shares=self.shares, page=self.num_index)
+        if int(content['read_status']) != 0:
+            return self.render("index.html", words=self.daywords[num_index], content=int(content['read_status'])-1, exist=exist, shares=shares, page=num_index)
         else:
-            return self.render("index.html", words=self.daywords[self.num_index], content="0", exist=self.exist, shares=self.shares, page=self.num_index)
+            return self.render("index.html", words=self.daywords[num_index], content="0", exist=exist, shares=shares, page=num_index)
 
 class OutHandler(BaseHandler):
     @tornado.web.authenticated
     def get(self):
         self.set_secure_cookie("user", "")
-        self.redirect('/login')
+        return self.redirect('/login')
 
 class SelectHandler(BaseHandler):
     @tornado.web.authenticated
     def get(self, *args, **kwargs):
-        self.status = self.get_argument("status")
-        self.user = self.get_current_user()
-        db.user.update({"account": self.user}, {"$set": {"user_status": self.status, "read_status": "0"}})
+        status = self.get_argument("status")
+        user = self.get_current_user()
+        db.user.update({"account": user}, {"$set": {"user_status": status, "read_status": "0"}})
         return self.render("count.html")
 
 class AltercountHandler(BaseHandler):
@@ -179,49 +177,49 @@ class AltercountHandler(BaseHandler):
 
     @tornado.web.authenticated
     def post(self):
-        self.count = self.get_argument("count")
-        self.user = self.get_current_user()
-        if not self.count:
+        count = self.get_argument("count")
+        user = self.get_current_user()
+        if not count:
             return self.write("输入不能为空")
-        db.user.update({"account": self.user}, {"$set": {"count": self.count}})
-        self.redirect('/')
+        db.user.update({"account": user}, {"$set": {"count": count}})
+        return self.redirect('/')
 
 class AddnoteHandler(BaseHandler):
     @tornado.web.authenticated
     def post(self, *args, **kwargs):
-        self.content = self.get_argument("content")
-        self.title = self.get_argument("title")
-        if not self.content or not self.title:
+        content = self.get_argument("content")
+        title = self.get_argument("title")
+        if not content or not title:
             return self.write("输入不能为空")
-        self.user = self.get_current_user()
-        self.auth = self.get_argument("auth")
+        user = self.get_current_user()
+        auth = self.get_argument("auth")
 
-        db.note.insert({"account": self.user, "title": self.title, "note": self.content, "share_status": self.auth, "time": self.time.asctime()})
+        db.note.insert({"account": user, "title": title, "note": content, "share_status": auth, "time": time.asctime()})
         return self.redirect('/')
 
 class ReselectHandler(BaseHandler):
     @tornado.web.authenticated
     def get(self, *args, **kwargs):
-        self.status = self.get_argument("status")
-        self.readcount = self.get_argument("readcount", None)
-        self.user = self.get_current_user()
-        self.content = db.user.find_one({"account": self.user})
+        status = self.get_argument("status")
+        readcount = self.get_argument("readcount", None)
+        user = self.get_current_user()
+        content = db.user.find_one({"account": user})
 
-        if self.status == "0":
-            db.user.update({"account": self.user}, {"$set": {"read_status": self.readcount}})
-        if self.status == "1":
-            db.user.update({"account": self.user}, {"$set": {"read_status": "0"}})
+        if status == "0":
+            db.user.update({"account": user}, {"$set": {"read_status": readcount}})
+        if status == "1":
+            db.user.update({"account": user}, {"$set": {"read_status": "0"}})
         return self.redirect('/')
 
 class ShownoteHandler(BaseHandler):
     @tornado.web.authenticated
     def get(self, *args, **kwargs):
-        self.title = self.get_argument("title")
-        self.auth = self.get_argument("account")
-        self.note = db.note.find_one({"account": self.auth, "title": self.title})
-        if not self.note:
+        title = self.get_argument("title")
+        auth = self.get_argument("account")
+        note = db.note.find_one({"account": auth, "title": title})
+        if not note:
             return self.write("笔记不存在")
-        return self.render("shownote.html", note=self.note)
+        return self.render("shownote.html", note=note)
 
 class Application(tornado.web.Application):
     def __init__(self):
